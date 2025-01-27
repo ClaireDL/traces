d2584e66294dd5feedd5ef0dcf382f6bdc9f6c51
ClaireDL <cl.delleluche@gmail.com>
2025-01-27 22:04:06 +0000

    Fixed scan to get complete path and added a simplified data structure

--- traces.py
+++ traces.py
@@ -3,52 +3,56 @@
 import json
 # import pandas as pd
 
-def scan(node):
+def get_node_data(node, data=tuple()):
+    """ Get the data we want to extract: operationName and duration for each node
+    """
+    node_operation_name = ""
+    node_duration = -1
+
+    # Checks if the keys of interest are present
+    if "operationName" in node and "duration" in node:
+        # Checks the type of the value for operationName
+        if isinstance(node.get("operationName"), str):
+            # Gets operationName for the node
+            node_operation_name = node.get("operationName")
+
+        # Checks the type of the value for duration
+        if isinstance(node.get("duration"), int):
+            # Gets the duration for the node
+            node_duration = node.get("duration")
+
+        # Creates a new tuple with the data we need
+        data = data + (node_operation_name, node_duration)
+
+    return data
+
+def scan(node, complete_path=""):
     """ Gets the function name when scanning
     """
     # Checks if node is a dictionary
     if isinstance(node, dict):
-        # Checks if the keys of interest are present
-        if "operationName" in node and "duration" in node:
-            # Checks the type of the value for operationName
-            if isinstance(node.get("operationName"), str):
-                # Gets operationName for the node
-                node_operation_name = node.get("operationName")
-
-            # Checks the type of the value for duration
-            if isinstance(node.get("duration"), int):
-                # Gets the duration for the node
-                node_duration = node.get("duration")
-
-            print("operation name: ", node_operation_name, " duration: ", node_duration)
-
+        # Gets operationName and duration
+        node_data = get_node_data(node)
+        if node_data:
+            # Gets the complete path by adding current operation_name to node path
+            complete_path = complete_path + "|" + node_data[0]
+            print("complete path: ", complete_path, " duration: ", node_data[1])
         # Iterates through the values in the dictionary
         for key, inner_node in node.items():
-            # Recursively call
-            scan(inner_node)
+            # Recursively calls scan
+            scan(inner_node, complete_path)
 
         # if "operationName" in node and "duration" in node:
         #     exit(1)
 
     if isinstance(node, list):
         for n in node:
-            scan(n)
-
-# A function with children has:
-# "hasChildren": true
-# followed by "childSpanIds" with a list of spanIds
+            scan(n, complete_path)
 
 print("Traces")
 
-with open('one_trace.json', 'r') as f:
+with open('trace_simplified.json', 'r') as f:
     all_traces = json.load(f)
     for trace in all_traces["data"]:
         scan(trace)
-
-# Print what we want in a table
-# 1 Function path is the succession of nodes/operation names
-# 2
-
-# How to write into a dataframe: dict or list then convert
-# df = pd.DataFrame(ds)
 
