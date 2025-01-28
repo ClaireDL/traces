f046e85a9b7de47109305e8ba75f675e4e7ef583
ClaireDL <cl.delleluche@gmail.com>
2025-01-28 23:14:57 +0000

    Added writing data structure into json file

--- traces.py
+++ traces.py
@@ -15,20 +15,18 @@ def get_node_data(node, data=tuple()):
         if isinstance(node.get("operationName"), str):
             # Gets operationName for the node
             node_operation_name = node.get("operationName")
-
         # Checks the type of the value for duration
         if isinstance(node.get("duration"), int):
             # Gets the duration for the node
             node_duration = node.get("duration")
-
         # Creates a new tuple with the data we need
         data = data + (node_operation_name, node_duration)
-
     return data
 
-def scan(node, complete_path=""):
+def scan(node, output='data.json', complete_path=""):
     """ Gets the function name when scanning
     """
+
     # Checks if node is a dictionary
     if isinstance(node, dict):
         # Gets operationName and duration
@@ -36,23 +34,29 @@ def scan(node, complete_path=""):
         if node_data:
             # Gets the complete path by adding current operation_name to node path
             complete_path = complete_path + "|" + node_data[0]
-            print("complete path: ", complete_path, " duration: ", node_data[1])
+            new_data = {'complete_path': complete_path, 'node_duration': node_data[1]}
+            file = open(output, 'a', encoding='utf-8')
+            file.write(str(new_data) + '\n')
+            print("writing: ", new_data)
+            file.close()
+            #print("complete path: ", complete_path, " duration: ", node_data[1])
+
         # Iterates through the values in the dictionary
         for key, inner_node in node.items():
             # Recursively calls scan
-            scan(inner_node, complete_path)
+            scan(inner_node, output, complete_path)
 
         # if "operationName" in node and "duration" in node:
         #     exit(1)
 
     if isinstance(node, list):
         for n in node:
-            scan(n, complete_path)
+            scan(n, output, complete_path)
 
 print("Traces")
 
-with open('trace_simplified.json', 'r') as f:
+with open('one_trace.json', 'r') as f:
     all_traces = json.load(f)
-    for trace in all_traces["data"]:
-        scan(trace)
 
+    for trace in all_traces["data"]:
+        scan(trace, 'one_trace_data_structure.json')
